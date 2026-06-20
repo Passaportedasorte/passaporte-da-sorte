59e853114c35c817bbf7f3e30ee8ee4c55630cf4
Bruno Petrikovski <petrikovskibruno@gmail.com>
2026-06-20 19:30:11 -0300

    add microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Script from "next/script";
 
 
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -100,6 +101,16 @@ export default function RootLayout({
     alt=""
   />
 </noscript>
+<Script id="clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xa7anzsupj");
+  `}
+</Script>
 </body>
     </html>
   );
